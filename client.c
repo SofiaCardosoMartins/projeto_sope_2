bb1512c67c6e2ae1e4a47be7a3f555f66be15d78
SofiaCardosoMartins <up201606033@g.uporto.pt>
2018-05-04 10:24:06 +0100

    Changed the start program name

--- client.c
+++ client.c
@@ -4,12 +4,16 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+  
   printf("** Running process %d (PGID %d) **\n", getpid(), getpgrp());
 
   if (argc == 4)
     printf("ARGS: %s | %s | %s\n", argv[1], argv[2], argv[3]);
 
   sleep(1);
+  
+
+
 
   return 0;
 }
