573b011fe013abab58a50135d458cbda4fab98c5
SofiaCardosoMartins <up201606033@g.uporto.pt>
2018-05-04 12:44:54 +0100

    Input treatment completed in client.c

--- client.c
+++ client.c
@@ -4,19 +4,34 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+void arrayChar_to_arrayInt(char* arrayChar,int* arrayInt)
+{
+  int preferedSeatNumber = 0;
+  char preferedSeat[WIDTH_SEAT] = {'\0'};
+
+  for(int i =0;i<=strlen(arrayChar);i++)
+  {
+    if((arrayChar[i] != ' ') && (arrayChar[i] != '\0'))
+      sprintf(preferedSeat,"%s%c",preferedSeat,arrayChar[i]);
+    else
+    {
+      arrayInt[preferedSeatNumber] = atoi(preferedSeat);
+      preferedSeatNumber++;
+      preferedSeat[0] = '\0';
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
-
-  printf("** Running process %d (PGID %d) **\n", getpid(), getpgrp());
-
   if (argc != 4)
     printf("Invalid argumments\n Usage: client <time_out> <num_wanted_seats> <pref_seat_list> \n");
 
   struct client_request rc;
-  rc.timeout_ms = stoi(*argv[1]);
-  rc.num_wanted_seats = stoi(*argv[2]);
-
-  sleep(1);
+  rc.timeout_ms = atoi(argv[1]);
+  rc.num_wanted_seats = atoi(argv[2]);
+  memset(rc.preferences,0,sizeof(rc.preferences));
+  arrayChar_to_arrayInt(argv[3],rc.preferences);
 
   return 0;
 }
--- client.h
+++ client.h
@@ -1,7 +1,12 @@
 #include "macros.h"
+#include "string.h"
+#include "stdlib.h"
 
 struct client_request {                  // delay in microseconds
   int timeout_ms;                       // client timeout in milliseconds
   int num_wanted_seats;                 // number of seats/tickets wanted
   int preferences[MAX_CLI_SEATS];       // list of preferences (seat numbers)
 };
+
+void arrayChar_to_arrayInt(char* arrayChar,int* arrayInt);
+int main(int argc, char *argv[]);
