d578f6e1ebad334f4f74765a2c768aef1a1ec674
Unknown <up201606214@g.uporto.pt>
2018-05-06 19:05:36 +0100

    Started server implementation

--- server.c
+++ server.c
@@ -1,6 +1,42 @@
 #include "server.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <unistd.h>
 
-int main()
+
+void * ticketOffice(void * arg){
+
+    return NULL;
+}
+
+
+int main(int argc, char *argv[])
 {
+if (argc != 4)
+    printf("Invalid argumments\n Usage: server <num_room_seats> <num_ticket_offices> <open_time> \n");
+
+int num_room_seats = atoi(argv[0]);
+int num_ticket_offices = atoi(argv[1]);
+int open_time = atoi(argv[2]);
+pthread_t threads[num_ticket_offices];
+
+int fd_req;
+
+mkfifo("requests", 0660);
+fd_req = open("requests", O_RDONLY);
+
+
+for(int i = 0; i < num_ticket_offices; i++){
+    pthread_create(&threads[i], NULL, ticketOffice, NULL);
+}
+
+
+close(fd_req);          //close the fifo
+unlink("requests");     //delete the fifo
+free("requests");
 return 0;
 }
